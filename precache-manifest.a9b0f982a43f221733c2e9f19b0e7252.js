4f6be462d328f2f7a7f9f5814061085f1a8885f1
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-06 00:37:47 +0300

    Updates

---
+++ precache-manifest.a9b0f982a43f221733c2e9f19b0e7252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d545a17eaa710b12a7201aa5ea6a2ff",
+    "url": "/react_layout_miami/index.html"
+  },
+  {
+    "revision": "0db270a7c6b67488c355",
+    "url": "/react_layout_miami/static/css/main.7098db07.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_layout_miami/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "0db270a7c6b67488c355",
+    "url": "/react_layout_miami/static/js/main.48616dd7.chunk.js"
+  },
+  {
+    "revision": "4dcf5508f0a9bed7a82c",
+    "url": "/react_layout_miami/static/js/runtime~main.59dfb4f6.js"
+  },
+  {
+    "revision": "4f75d12689b3f338bb51e7d934c95638",
+    "url": "/react_layout_miami/static/media/header-bg.4f75d126.png"
+  },
+  {
+    "revision": "63a8d2c1790522524a610d31b46432ac",
+    "url": "/react_layout_miami/static/media/nastol.com.63a8d2c1.png"
+  }
+]);
